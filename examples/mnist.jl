35ab031960a25ca52fa0e232b787a341831e1083
Pedro <pedromxavier@poli.ufrj.br>
2021-11-10 10:06:51 -0300

    Update Examples

--- examples/mnist.jl
+++
@@ -1,9 +0,0 @@
-using WiSARD
-using DataFrames
-import CSV
-
-# First, retrive MNIST .csv data from
-# https://www.kaggle.com/oddrationale/mnist-in-csv
-
-train_df = CSV.read(joinpath(@__DIR__, "mnist_train.csv"), DataFrame)
-test_df = CSV.read(joinpath(@__DIR__, "mnist_test.csv"), DataFrame)
