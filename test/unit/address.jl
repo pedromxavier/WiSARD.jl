260f6943738e96841882ea3043108d6019aef321
Pedro Maciel Xavier <pedromxavier@poli.ufrj.br>
2022-10-22 02:37:21 -0300

    Fix test

--- test/unit/address.jl
+++ test/unit/address.jl
@@ -4,9 +4,9 @@ function test_address()
 
     x = [1, 0, 0, 1, 0, 1, 0, 1, 0]
 
-    @test WiSARD.address(wnn, x, 1) == UInt(1)
-    @test WiSARD.address(wnn, x, 2) == UInt(5)
-    @test WiSARD.address(wnn, x, 3) == UInt(2)
+    @test WiSARD.address(wnn, x, 1) == WiSARD.WNNKEY{UInt}(1, UInt(1))
+    @test WiSARD.address(wnn, x, 2) == WiSARD.WNNKEY{UInt}(2, UInt(5))
+    @test WiSARD.address(wnn, x, 3) == WiSARD.WNNKEY{UInt}(3, UInt(2))
 
     return nothing
 end
